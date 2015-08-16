7088cba2cef339565b01b7f3023d6532d7cf69c9
Mike DiGiovanni <mike@mikedg.com>
2015-08-16 15:54:56 -0400

    Added signature test.
    Known issue with getting debug to sign with a custom key.
    Setup flavors to one fails every test and the other passes.

--- app/src/androidTest/java/com/mikedg/continuitytests/TestingUtil.java
+++ app/src/androidTest/java/com/mikedg/continuitytests/TestingUtil.java
@@ -2,18 +2,35 @@ package com.mikedg.continuitytests;
 
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.os.Binder;
 import android.support.test.InstrumentationRegistry;
 
 /**
  * Created by vinbl on 8/16/2015.
  */
 public class TestingUtil {
-    public static final boolean doesPackageHavePermission(String permission, String packageName) {
+    private static final PackageManager getPackageManager() {
         Context context = InstrumentationRegistry.getTargetContext();
         PackageManager packageManager = context.getPackageManager();
 
-        int hasPermission = packageManager.checkPermission(permission, packageName);
+        return packageManager;
+    }
+
+    public static final boolean doesPackageHavePermission(String permission, String packageName) {
+        int hasPermission = getPackageManager().checkPermission(permission, packageName);
 
         return hasPermission == PackageManager.PERMISSION_GRANTED;
+    }
+
+    public static final boolean doesSignatureMatch(String characterSignature) throws PackageManager.NameNotFoundException {
+        PackageManager packageManager = getPackageManager();
+        Signature[] signatures = packageManager.getPackageInfo(packageManager.getNameForUid(Binder.getCallingUid()), PackageManager.GET_SIGNATURES).signatures;
+
+        if (signatures.length != 1) {
+            return false; //Right now we only check a single signature
+        }
+
+        return characterSignature.equals( signatures[0].toCharsString());
     }
 }
